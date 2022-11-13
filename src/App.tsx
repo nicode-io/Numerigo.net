663a685bad2c4023b1f56e8c6e1343c45538cf5f
Nicolas Denoël <nicolas.denoel@domintell.com>
2022-11-13 21:34:23 +0100

    Test build on host server

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Numerigo.net - Multiple NGINX test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
